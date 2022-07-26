c63e720736b0899727a8f5c11305f70da52cbc7c
Rongmario <rongmario@protonmail.com>
2022-07-26 22:39:41 +0100

    First setup

---
+++ src/main/java/com/cleanroommc/ponder/Ponder.java
@@ -0,0 +1,12 @@
+package com.cleanroommc.ponder;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Ponder.ID, name = Ponder.NAME, version = Ponder.VERSION)
+public class Ponder {
+
+    public static final String ID = "ponder";
+    public static final String NAME = "Ponder";
+    public static final String VERSION = "1.0";
+
+}
